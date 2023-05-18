0cb9069985c2b1a038f236da6dd5e2c43c17b57b
Marcelo Becker Koche <180504052@senacinformatica.br>
2023-05-17 21:41:53 -0300

    1 parte

--- AssistenciaTecnica/src/View/View.java
+++ AssistenciaTecnica/src/View/View.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package View;
 
 /**
  *
  * @author 180504052
  */
-public class Funcionario {
+public class View {
     
 }
